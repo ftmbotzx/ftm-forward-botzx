f58dd210977a37e92ca26db02f74a9361f53ab9d
ftmbotzx <kumarasta91@gmail.com>
2025-10-01 21:29:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ class Config:
     # Required environment variables - no defaults for security
     API_ID = environ.get("API_ID", "28776072")
     API_HASH = environ.get("API_HASH", "b3a786dce1f4e7d56674b7cadfde3c9d")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "7789900726:AAESZGRaUsLGwg2JoTz-gPcfoL0GTi2x3j4") 
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7809869650:AAF5O8Luy7kFRyqh-ZE4KESgGQM8XvOcxyI") 
     BOT_SESSION = environ.get("BOT_SESSION", "forward-bot") 
     DATABASE_URI = environ.get("DATABASE", "mongodb+srv://ftm:ftm@cluster0.9a4gw2t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "forward-bot")
-    OWNER_ID_STR = environ.get("OWNER_ID", "7744665378")
+    OWNER_ID_STR = environ.get("OWNER_ID", "7744665378 7810783444")
     
     # Validate required environment variables
     @classmethod
